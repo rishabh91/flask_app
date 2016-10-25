dae64a72626a68db9ff2551d1e7decf1cf932cb0
Rishabh <rishabh.aditya@hotmail.com>
2016-10-25 14:07:36 +0530

    Now the dictionary will show two meanings
    1)Now the dictionary will show two meanings
    2) Handles the scenarios if no meaning exists

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 import requests
 from flask import Flask, render_template, request, url_for
 
@@ -19,23 +20,34 @@ def dictionary():
 	#getting the word from the user to search meaning from pearson API
 	word=request.form['word']
 	#building the URL for making the API call
-	url = "http://api.pearson.com/v2/dictionaries/ldoce5/entries?headword=" + word + "&limit=1"
-	r = requests.get(url) # making a get call to the API to fetch the JSON values
-	data = r.json()
-	value = data['results']
-	value = value[0]
-	value = value['senses']
-	value = value[0]
-	try:
-		value = value['subsenses']
-		value = value[0]
-		value = value['definition']
-	except:
-		value = value['definition']
+	url = "http://api.pearson.com/v2/dictionaries/ldoce5/entries?headword="+word+"&limit=2"
+	r = requests.get(url)
+	code = r.status_code
+	if code==200:
+		data = r.json()
+		results = data['results']
+		if len(results)>0:
+			values =[]
+			for result in results:
+				value = result['senses']
+				value = value[0]
+				try:
+					value = value['subsenses']
+					value = value[0]
+					value = value['definition']
+					values.append(value)
+				except:
+					value = value['definition']
+					values.append(value)
+		else:
+			return render_template('form_nodata.html')
+	else:
+		return render_template('form_nodata.html')
 	
 
-	return render_template('form_action.html', meaning=value)
+	return render_template('form_action.html', meaning=values)
 
 # Run the app :)
 if __name__ == '__main__':
-	app.run()
+	port = int(os.environ.get("PORT", 5000))
+	app.run(host='0.0.0.0', port=port)
